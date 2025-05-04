f08dd39c866d884c9e47746bad3cfb8e351153d2
Mr Aung Ko <mr.aungkokooo.mmict@gmail.com>
2025-05-05 06:28:57 +0630

    change java test3!

--- src/test/java/com/mycompany/app/MyWebAppApplicationTests.java
+++ src/test/java/com/mycompany/app/MyWebAppApplicationTests.java
@@ -3,7 +3,7 @@ package com.mycompany.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = MyWebAppApplication.class)
+@SpringBootTest(classes = HelloController.class)
 class MyWebAppApplicationTests {
 
     @Test
