306143f457c7eaa8a2a5a382ce8c03f59a0221f0
Mr Aung Ko <mr.aungkokooo.mmict@gmail.com>
2025-05-04 22:16:38 +0630

    change java test2!

--- src/main/java/com/mycompany/app/HelloController.java
+++ src/main/java/com/mycompany/app/HelloController.java
@@ -1,12 +1,13 @@
 package com.mycompany.app;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+   import org.springframework.web.bind.annotation.GetMapping;
+   import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class HelloController {
+   @RestController
+   public class HelloController {
 
-    public static void main(String[] args) {
-        SpringApplication.run(HelloController.class, args);
-    }
-}
+       @GetMapping("/hello")
+       public String sayHello() {
+           return "Hello from Spring Boot!";
+       }
+   }
